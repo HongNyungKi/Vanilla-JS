19b3c9ca456b4719562871d1b6325bf94c7f6190
HongNyungKi <hnk2017@naver.com>
2020-02-24 18:10:26 +0900

    learn to js

--- 3_Make/name.js
+++ 3_Make/name.js
@@ -25,3 +25,22 @@ function init() {
 }
 
 init();
+
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
